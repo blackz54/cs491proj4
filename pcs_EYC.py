e353775de734419e46866817f86e46436bd91876
ElaineYChu <elainechu@nevada.unr.edu>
2019-11-20 13:48:25 -0800

    Elaine's versionof pcs.py

---
+++ pcs_EYC.py
@@ -0,0 +1,66 @@
+#### PROJECT 4 - EYC
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+X = np.array([1,1,2,2,2,3,3,3,4,4,5,5,5],[1,0,2,1,4,4,3,2,4,5,5,7,4]])
+X = X.T
+
+## Take data matrix X and standardize given centering and scaling
+def compute_Z(X, centering=True, scaling=False):
+    Z = X
+
+    if centering == True:
+        mean_mat = np.mean(Z,axis=0)
+        Z = X - mean_mat
+        
+    if scaling == True:
+        stdev = np.std(X,axis=0)
+        Z = np.divide(Z,stdev)
+    
+    return(Z)
+
+
+## Take standardized matrix Z and return covariance matrix Z.T*Z
+def compute_covariance_matrix(Z):
+    Ztp = Z.T
+    
+    COV = np.dot(Ztp,Z)
+    
+    return(COV)
+
+
+## Take covariance matrix and return ordered PCS and corresponding L
+def find_pcs(COV):
+    L, PCS = np.linalg.eig(COV)
+    
+    idx = L.argsort()[::-1]
+    L = L[idx]
+    PCS = PCS[:,idx]
+    
+    return(PCS, L)
+
+
+
+## Take inputted data and return projected data, Z_star given k and var
+def project_data(Z, PCS, L, k, var):
+    eucld = np.linalg.norm(PCS, axis=0)
+    PCS_norm = PCS/eucld
+    
+    if k==0:
+        cuml = 0
+        for i in range(len(L)):
+            cuml += L[i]
+            k += 1
+            if (cuml/sum(L) >= var):
+                PCS_new = PCS_norm[:,:k]
+    
+    if var==0:
+        PCS_new = PCS_norm[:,:k]
+        
+    feat_vec = PCS_new.T
+    Z_adj = Z.T
+    
+    Z_star = np.dot(feat_vec, Z_adj)    
+    
+    return(Z_star)
